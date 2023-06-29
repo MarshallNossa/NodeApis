810f2a071311ea6ca321ad4100fc2b5cb29dfa5e
MarshallNossa <infinitymc777@gmail.com>
2023-06-29 08:19:58 -0500

    feat: :sparkles: Consumo de api con https
    Realizamos el consumo de una api con https

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 import {createServer} from "http";
+import https from "https";
 
 //Creamos el servidor, lo metemos en una variable y le mandamos un mensaje
 let myServer = createServer((req, res)=>{
-    let datos = "";
+    //Hacemos la consulta a la api con https
+    https.get("https://pokeapi.co/api/v2/pokemon/ditto", (req)=>{
+        //Esperamos la respuesta de la api
+        let data = "";
+        req.on("data", (chunk)=>{
+            data += chunk;
+        })
+        //Obtenemos la respuesta y la mostramos en consola
+        req.on("end", ()=>{
+            console.log(JSON.parse(data));
+        })
+    })
+    res.end("hola");
+
+
+
+
+    /* let datos = "";
     //Le decimos al request que renderice todo lo que entre y cuando finaliza manda la promesa y la muestra
     req.on("data", (chunk)=>{
         datos += chunk;
     })
     req.on("end", ()=>{
         res.end(datos);
-    })
+    }) */
 })
 
 //Creamos una variable con la configuración del server
